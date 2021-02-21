f88dd7dc076fb53cb736705c4513d90621c924f4
= <=>
2021-02-21 13:35:56 -0300

    added blast database formatting

--- time_benchmark.sh
+++ time_benchmark.sh
@@ -6,6 +6,8 @@ echo "NESS  (4 threads)"
 echo ''
 time python3 ness.py search --input tests/data/query.fasta --model  tests/data/uniprot_sprot.model --database tests/data/uniprot_sprot --output tests/data/uniprot_sprot.csv
 
+makeblastdb -in tests/data/uniprot_sprot.fasta -dbtype prot -out tests/data/uniprot_sprot
+
 echo "BLAST (1 thread)"
 echo ''
 time blastp -query tests/data/query.fasta -db tests/data/uniprot_sprot -out tests/data/teste.txt > /dev/null
